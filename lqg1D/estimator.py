10d1870d0685606434ce827e72ae13e0b7e1b557
PietroMelzi <pietromelzi95@gmail.com>
2019-09-15 12:28:07 +0200

    Macrostates with no constant intervals

--- lqg1D/estimator.py
+++ lqg1D/estimator.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def get_mcrst(state, min, max, n_states):
+def get_mcrst_const(state, min, max, n_states):
     if state == min:
         return 0
     h = (max - min) / n_states
@@ -10,19 +10,31 @@ def get_mcrst(state, min, max, n_states):
         index = index + 1
     return index - 1
 
+def get_mcrst_not_const(state, intervals):
+    index = 0
+    for int in intervals:
+        if int[0] <= state < int[1]:
+            return index
+        else:
+            index = index + 1
 
-def estimate_mcrst_dist(samples, n_macrostates, regular, min_state, max_state, intervals=None):
+
+def estimate_mcrst_dist(samples_state, n_macrostates, constant_intervals, min_state, max_state, intervals=None):
     # regular is a boolean telling if all the macrostates have the same dimension
     accumulator = np.zeros(n_macrostates)
-    if regular:
-        index = [get_mcrst(sample, min_state, max_state, n_macrostates) for sample in samples]
-        for i in index:
-            accumulator[i] = accumulator[i] + 1
-
-        # to avoid estimates equal to zero
-        zeros = len(accumulator) - np.count_nonzero(accumulator)
-        accumulator = map(lambda a: 1 if a == 0 else a, accumulator)
-        return [a / (len(samples) + zeros) for a in accumulator]
+    if constant_intervals:
+        index = [get_mcrst_const(sample, min_state, max_state, n_macrostates) for sample in samples_state]
     else:
-        pass
-        # TODO
+        index = [get_mcrst_not_const(sample, intervals) for sample in samples_state]
+
+    for i in index:
+        accumulator[i] = accumulator[i] + 1
+
+    # to avoid estimates equal to zero
+    zeros = len(accumulator) - np.count_nonzero(accumulator)
+    accumulator = map(lambda a: 1 if a == 0 else a, accumulator)
+    return [a / (len(samples_state) + zeros) for a in accumulator]
+    # TODO
+
+
+
--- lqg1D/lqgspo.py
+++ lqg1D/lqgspo.py
@@ -3,11 +3,12 @@ import potion.envs
 from lqg1D.policy import StochasticPolicy as sp, DeterministicPolicy as dp
 from lqg1D import estimator as e
 import torch
+import random
 
 env = gym.make('LQG1D-v0')
 N_SAMPLES = 2000
 N_MACROSTATES = 5
-N_MAX_STEPS = 20
+N_STEPS = 20
 INIT_DETERMINISTIC_PARAM = -0.2
 
 # constants for stochastic policy
@@ -15,52 +16,67 @@ INIT_MU = 0.
 INIT_OMEGA = 1.
 INIT_LR = 0.01
 
-det_pol = dp(INIT_DETERMINISTIC_PARAM)
-samples = []
+# about macrostates
+CONSTANT_INTERVALS = False
+INTERVALS = [[-2, -0.4], [-0.4, -0.1], [-0.1, 0.1], [0.1, 0.4], [0.4, 2]]
 
-for i in range(0, N_SAMPLES):
-    env.reset()
-    episode = []
 
-    for j in range (0, N_MAX_STEPS):
-        state = env.get_state()
-        det_pol.train()
-        action = det_pol(torch.from_numpy(state).float())
-        new_state, r, done, info = env.step(action.detach().numpy())
+def sampling_phase():
+    det_pol = dp(INIT_DETERMINISTIC_PARAM)
+    samples_list = []
+    for i in range(0, N_SAMPLES):
+        env.reset()
 
-        # print("[{}] - State = {}, Action = {}, Reward = {}, Next state = {}".format(i, state, action.detach(
-        # ).numpy(), r, new_state)) store each step I get
-        episode.append([state[0], action[0], r, new_state[0]])
-        state = new_state
+        for j in range(0, N_STEPS):
+            state = env.get_state()
+            det_pol.train()
+            action = det_pol(torch.from_numpy(state).float())
+            new_state, r, done, info = env.step(action.detach().numpy())
 
-    # store each episode I get
-    samples.append(episode)
+            # print("[{}] - State = {}, Action = {}, Reward = {}, Next state = {}".format(i, state, action.detach(
+            # ).numpy(), r, new_state))
+            # store each step I get
+            samples_list.append([state[0], action[0], r, new_state[0]])
 
-state_sampled = []
-for sam in samples:
-    for ep in sam:
-        state_sampled.append(ep[0])
+    random.shuffle(samples_list)
+    return samples_list
 
-# estimate of the macrostate distribution using the samples I have
-estimate_mcrst_dist = e.estimate_mcrst_dist(state_sampled, N_MACROSTATES, True, -env.max_pos, env.max_pos)
 
-# let's calculate a different stochastic policy for every macrostate
-st_policy = []
-for i in range(0, N_MACROSTATES):
-    st_policy.append(sp(INIT_MU, INIT_OMEGA, INIT_LR))
+def get_states_from_samples():
+    return [sam[0] for sam in samples]
 
-for i in range(0, N_SAMPLES):
-    for j in range(0, len(samples[i])):
-        mcrst = e.get_mcrst(samples[i][j][0], -env.max_pos, env.max_pos, N_MACROSTATES)
 
-        grad_log_pol_mu, grad_log_pol_omega = st_policy[mcrst].gradient_log_policy(samples[i][j][1])
+def updating_phase():
+    # estimate the macrostate distribution using the samples I have
+    estimate_mcrst_dist = e.estimate_mcrst_dist(get_states_from_samples(), N_MACROSTATES, CONSTANT_INTERVALS,
+                                                -env.max_pos, env.max_pos, INTERVALS)
+    print(estimate_mcrst_dist)
+    for s in samples:
+        mcrst = e.get_mcrst_const(s[0], -env.max_pos, env.max_pos, N_MACROSTATES) if CONSTANT_INTERVALS else \
+            e.get_mcrst_not_const(s[0], INTERVALS)
+
+        grad_log_pol_mu, grad_log_pol_omega = st_policy[mcrst].gradient_log_policy(s[1])
+        # grad = [st.gradient_log_policy(samples[i][j][1]) for st in st_policy]
 
         # quantities used to perform gradient ascent
         grad_mu = grad_log_pol_mu / estimate_mcrst_dist[mcrst]
         grad_omega = grad_log_pol_omega / estimate_mcrst_dist[mcrst]
         st_policy[mcrst].update_parameters(grad_mu, grad_omega)
 
+
+def show_results():
+    for i in range(0, N_MACROSTATES):
+        par = st_policy[i].parameters()
+        print("[MCRST{}]".format(i))
+        print([p for p in par])
+
+
+samples = sampling_phase()
+
+# let's calculate a different stochastic policy for every macrostate
+st_policy = []
 for i in range(0, N_MACROSTATES):
-    par = st_policy[i].parameters()
-    print("[MCRST{}]".format(i))
-    print([p for p in par])
+    st_policy.append(sp(INIT_MU, INIT_OMEGA, INIT_LR))
+
+updating_phase()
+show_results()

6217fa98e80e1d31b7f0c7cfc0c23ba7f05dcfb7
PietroMelzi <pietro.melzi@mail.polimi.it>
2020-01-12 12:07:48 +0100

    minigolf visualizer

--- DPO/visualizer/minigolf_visualizer.py
+++ DPO/visualizer/minigolf_visualizer.py
@@ -18,6 +18,8 @@ class MGVisualizer(Visualizer):
         self.title = title
         self.filename = filename
         plt.figure(figsize=(12.8, 9.6))
+        self.k = ['w1', 'w2', 'w3', 'w4', 'fail', 'j']
+        self.titles = ["w1", "w2", "w3", "w4", "Failures", "Performance"]
 
     def show_values(self, w, j, fail):
 
@@ -32,41 +34,13 @@ class MGVisualizer(Visualizer):
 
         plt.subplots_adjust(hspace=0.6, wspace=0.4)
 
-        plt.subplot(3, 2, 1)
-        plt.title("W1")
-        plt.plot(self.w1, label="W1")
-        plt.annotate(np.round(self.w1[-1], decimals=3), (len(self.w1) - 1, self.w1[-1]))
-        plt.grid(b=True)
-
-        plt.subplot(3, 2, 2)
-        plt.title("W2")
-        plt.plot(self.w2, label="W2")
-        plt.annotate(np.round(self.w2[-1], decimals=3), (len(self.w2) - 1, self.w2[-1]))
-        plt.grid(b=True)
-
-        plt.subplot(3, 2, 3)
-        plt.title("W3")
-        plt.plot(self.w3, label="W3")
-        plt.annotate(np.round(self.w3[-1], decimals=3), (len(self.w3) - 1, self.w3[-1]))
-        plt.grid(b=True)
-
-        plt.subplot(3, 2, 4)
-        plt.title("W4")
-        plt.plot(self.w4, label="W4")
-        plt.annotate(np.round(self.w4[-1], decimals=3), (len(self.w4) - 1, self.w4[-1]))
-        plt.grid(b=True)
-
-        plt.subplot(3, 2, 5)
-        plt.title("Failures")
-        plt.plot(self.fail, label="Failures")
-        plt.annotate(np.round(self.fail[-1], decimals=3), (len(self.fail) - 1, self.fail[-1]))
-        plt.grid(b=True)
-
-        plt.subplot(3, 2, 6)
-        plt.title("J")
-        plt.plot(self.j, label="J")
-        plt.annotate(np.round(self.j[-1], decimals=3), (len(self.j) - 1, self.j[-1]))
-        plt.grid(b=True)
+        var = [self.w1, self.w2, self.w3, self.w4, self.fail, self.j]
+        for i in range(6):
+            ax = plt.subplot(3, 2, i+1)
+            plt.title(self.titles[i])
+            plt.plot(var[i], label=self.titles[i])
+            plt.text(1.02, 0.9, np.round(var[i][-1], decimals=3), transform=ax.transAxes)
+            plt.grid(b=True)
 
         plt.draw()
         plt.pause(0.001)
@@ -82,52 +56,18 @@ class MGVisualizer(Visualizer):
     def show_average(self, avg, std):
 
         plt.figure(figsize=(12.8, 9.6))
-        plt.suptitle("Averages and confidence")
+        plt.suptitle("Average and confidence")
         plt.subplots_adjust(hspace=0.6, wspace=0.4)
 
         x = [n * 10 for n in range(0, len(avg['w1']))]
 
-        plt.subplot(3, 2, 1)
-        plt.title("W1")
-        plt.annotate(np.round(avg['w1'][-1], decimals=3), ((len(avg['w1']) - 1) * 10, avg['w1'][-1]))
-        std_resized = np.resize([2*s for s in std['w1']], (len(std['w1']), ))
-        plt.errorbar(x, avg['w1'], yerr=std_resized)
-        plt.grid(b=True)
-
-        plt.subplot(3, 2, 2)
-        plt.title("W2")
-        plt.annotate(np.round(avg['w2'][-1], decimals=3), ((len(avg['w2']) - 1) * 10, avg['w2'][-1]))
-        std_resized = np.resize([2 * s for s in std['w2']], (len(std['w2']),))
-        plt.errorbar(x, avg['w2'], yerr=std_resized)
-        plt.grid(b=True)
-
-        plt.subplot(3, 2, 3)
-        plt.title("W3")
-        plt.annotate(np.round(avg['w3'][-1], decimals=3), ((len(avg['w3']) - 1) * 10, avg['w3'][-1]))
-        std_resized = np.resize([2 * s for s in std['w3']], (len(std['w3']),))
-        plt.errorbar(x, avg['w3'], yerr=std_resized)
-        plt.grid(b=True)
-
-        plt.subplot(3, 2, 4)
-        plt.title("W4")
-        plt.annotate(np.round(avg['w4'][-1], decimals=3), ((len(avg['w4']) - 1) * 10, avg['w4'][-1]))
-        std_resized = np.resize([2 * s for s in std['w4']], (len(std['w4']),))
-        plt.errorbar(x, avg['w4'], yerr=std_resized)
-        plt.grid(b=True)
-
-        plt.subplot(3, 2, 5)
-        plt.title("Failures")
-        plt.annotate(np.round(avg['fail'][-1], decimals=3), ((len(avg['fail']) - 1) * 10, avg['fail'][-1]))
-        std_resized = np.resize([2 * s for s in std['fail']], (len(std['fail']),))
-        plt.errorbar(x, avg['fail'], yerr=std_resized)
-        plt.grid(b=True)
-
-        plt.subplot(3, 2, 6)
-        plt.title("J")
-        plt.annotate(np.round(avg['j'][-1], decimals=3), ((len(avg['j']) - 1) * 10, avg['j'][-1]))
-        std_resized = np.resize([2 * s for s in std['j']], (len(std['j']),))
-        plt.errorbar(x, avg['j'], yerr=std_resized)
-        plt.grid(b=True)
+        for i in range(6):
+            ax = plt.subplot(3, 2, i+1)
+            plt.title(self.titles[i])
+            plt.text(1.02, 0.9, np.round(avg[self.k[i]][-1], decimals=3), transform=ax.transAxes)
+            std_resized = np.resize([2*s for s in std[self.k[i]]], (len(std[self.k[i]]), ))
+            plt.errorbar(x, avg[self.k[i]], yerr=std_resized)
+            plt.grid(b=True)
 
         plt.draw()
         plt.pause(0.001)
